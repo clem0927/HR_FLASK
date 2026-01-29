2228500309cc24753bf5c7eb4721183a3f0df3c9
김경은 <20221301@daejin.ac.kr>
2026-01-30 05:58:26 +0900

    근태 이상 감지 AI완료

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,18 @@
 # src/__init__.py
 from flask import Flask
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
 
     from src.young.chatbot.routes import bp as chatbot_bp
     from src.young.regression.routes import bp as regression_bp
+    from src.eun.attendance.routes import bp as attendance_bp
 
     app.register_blueprint(chatbot_bp)
     app.register_blueprint(regression_bp)
+    app.register_blueprint(attendance_bp)
 
     return app
